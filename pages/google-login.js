38f20b281acde462e7404583341fb4190dfdd86f
pramiedevs <pramietest@gmail.com>
2024-10-23 21:04:18 +0000

    google id updated

--- pages/google-login.js
+++ pages/google-login.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const clientId = 'YOUR_CLIENT_ID'; // Replace with your client ID
+    const clientId = '425627947718-26j5n0t5t3kme55govd3n463ogolfjbo.apps.googleusercontent.com'; // Replace with your client ID
     const userPic = document.getElementById('user-pic');
     const userInfo = document.getElementById('user-info');
     const signinButton = document.getElementById('g-signin-button'); // Hidden button
