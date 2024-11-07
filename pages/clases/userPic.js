630e9e01ed31cf59645c50e90dda6301033a4cd1
pramiedevs <pramietest@gmail.com>
2024-11-07 19:15:11 +0000

    user pic for innner site

--- pages/clases/userPic.js
+++ pages/clases/userPic.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const userPic = document.getElementById('user-pic');
     
     // Retrieve the URL from localStorage
-    const profilePictureURL = localStorage.getItem('userPic');
+    const profilePictureURL = sessionStorageStorage.getItem('userPic');
     
     if (profilePictureURL) {
         userPic.src = profilePictureURL;
@@ -12,9 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.getElementById('logout-button').addEventListener('click', () => {
-    // Clear the user picture URL from localStorage
-    localStorage.removeItem('userPic');
-    
     // Redirect back to clases.html
     window.location.href = '../clases.html';
 });
